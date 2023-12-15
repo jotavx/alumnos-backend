95beef926770af1c022b961777f7bf5520dcb93f
jotavx <jotaviarruel97@gmail.com>
2023-12-15 02:03:08 -0300

    First Commit

---
+++ controllers/correoController.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { request: httpRequest } = require("http");
+const nodeMailer = require("nodemailer");
+
+const envioCorreo = (req = request, resp = response) => {
+  let body = req.body;
+
+  let listaCorreos = body.email;
+
+  let config = nodeMailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+      user: "jvcode7@gmail.com",
+      pass: "jbof flny cdqn xtdl",
+    },
+  });
+
+  const opciones = {
+    from: "Prueba",
+    subject: body.asunto,
+    to: listaCorreos,
+    text: body.mensaje,
+  };
+
+  config.sendMail(opciones, function (error, result) {
+    if (error) return resp.json({ ok: false, msg: error });
+
+    return resp.json({
+      ok: true,
+      msg: result,
+    });
+  });
+};
+
+module.exports = {
+  envioCorreo,
+};
